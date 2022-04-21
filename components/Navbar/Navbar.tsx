9dcae3c5f34f2bef3b6304f8e460608fe035899e
Oghenemano <oghenemano@MacBook-Pro.local>
2022-04-22 01:17:11 +0300

    making list in the navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,16 +1,29 @@
-import React from 'react'
-import { AppBar,Typography,Link  } from '@mui/material'
+import React,{useState} from 'react'
+import { AppBar,Typography,Link,Box, ListItemText  } from '@mui/material'
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import useStyles from './styles'
+import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown';
 
 export default function Navbar() {
+    const [open, setOpen] = useState(false)
     const classes = useStyles()
   return (
     <div>
         <AppBar className={classes.root}  >
-            <div className='navbar__container '>
+            <div className='navbar__container'>
                <Link className={classes.link}  > <Typography variant='body1'>Trending</Typography></Link>
             <Link  className={classes.link}  ><Typography variant='body1'>Houses/Rent</Typography></Link>
             <Link className={classes.link} > <Typography  variant='body1'>Best Sellers</Typography></Link>
+            
+            <ListItemText >Help <KeyboardArrowDown
+                  sx={{
+                 
+                    opacity: 2,
+                    transform: open ? 'rotate(-180deg)' : 'rotate(0)',
+                    transition: '0.2s',
+                  }}
+                /></ListItemText>
+        
             </div>
         </AppBar>
     </div>
