874a4ff9a0a8664befd4f82594c8e8cedb591802
Oghenemano <oghenemano@MacBook-Pro.local>
2022-04-27 00:42:00 +0300

    fixed the navbar responsivenes

---
+++ components/AvailableServices/AvailableService.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Paper from '@mui/material/Paper'
+import { Typography } from '@mui/material'
+type Props = {}
+
+const AvailableService = (props: Props) => {
+  return (
+    <div>
+        <Paper elevation={2}  sx={{height:200, background:"whitesmoke"}}>
+            <div style={{display:"flex", justifyContent:"center"}}>
+                <Typography variant='h5'>Available Service for you</Typography>
+
+   
+            </div>
+            </Paper></div>
+  )
+}
+export default AvailableService
