bb5a10666374c6334a330213ba411317e6f2f9bb
Oghenemano <oghenemano@MacBook-Pro.local>
2022-04-26 19:59:50 +0300

    finish the footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -33,62 +33,68 @@ export default function Footer({}: Props) {
           </div>
           
          
-          <div style={{display:"flex",marginTop:"1%",justifyContent:"space-around"}}>
+          <div style={{display:"grid", marginLeft:"10%", gridTemplateColumns:"auto auto auto auto"}}>
             <StyledTypography variant="caption">LET US HELP YOU</StyledTypography>
 
-            <StyledTypography variant="caption">ABOUT US</StyledTypography>
+            <StyledTypography  sx={{marginLeft:"-10px"}} variant="caption">ABOUT US</StyledTypography>
 
             <StyledTypography variant="caption">MAKE MONEY WITH US</StyledTypography>
 
             <StyledTypography variant="caption">MORE INFO</StyledTypography>
           </div>
          
-          <div style={{display:"flex",justifyContent:"space-around"}}>
+          <div style={{display:"grid",gap:"40px", marginLeft:"10%", gridTemplateColumns:"auto auto auto auto"}}>
             <StyledTypography variant="caption">Help center</StyledTypography>
 
-            <StyledTypography variant="caption">Our Blog</StyledTypography>
+            <StyledTypography sx={{marginLeft:"10px"}} variant="caption">Our Blog</StyledTypography>
 
-            <StyledTypography variant="caption">Sell on this website</StyledTypography>
+            <StyledTypography   sx={{position:"relative", right:10}} variant="caption">Sell on this website</StyledTypography>
 
             <StyledTypography variant="caption">Privacy Policy</StyledTypography>
           </div>
-         
-          <div style={{display:"flex",justifyContent:"space-around"}}>
+            <div style={{display:"grid",gap:"20px", marginLeft:"10%", gridTemplateColumns:"auto auto auto auto"}}>
             <StyledTypography variant="caption">Your Account</StyledTypography>
 
             <StyledTypography variant="caption">Career</StyledTypography>
 
             <StyledTypography variant="caption">Advertise</StyledTypography>
 
-            <StyledTypography variant="caption">Authentic Item Policy</StyledTypography>
+            <StyledTypography sx={{position:"relative", right:-57}} variant="caption">Authentic Item Policy</StyledTypography>
           </div>
          
-          <div style={{display:"flex",justifyContent:"space-around"}}>
+          <div style={{display:"grid", gap:"20px",marginLeft:"10%", gridTemplateColumns:"auto auto auto auto"}}>
             <StyledTypography variant="caption">Assistance</StyledTypography>
 
-            <StyledTypography variant="caption">Forum</StyledTypography>
+            <StyledTypography sx={{position:"relative", right:4}} variant="caption">Forum</StyledTypography>
 
-            <StyledTypography variant="caption">Host here</StyledTypography>
+            <StyledTypography  sx={{marginLeft:"-20px"}} variant="caption">Host here</StyledTypography>
 
-            <StyledTypography variant="caption">Cookie Policy</StyledTypography>
+            <StyledTypography sx={{position:"relative", right:-35}} variant="caption">Cookie Policy</StyledTypography>
           </div>
          
-          <div style={{display:"flex",justifyContent:"space-around"}}>
+          <div style={{display:"grid", marginLeft:"10%", gridTemplateColumns:"auto auto auto auto"}}>
             <StyledTypography variant="caption">Manage your content & device</StyledTypography>
 
-            <StyledTypography variant="caption">Terms & Conditions</StyledTypography>
+            <StyledTypography sx={{position:"relative", left:-42}} variant="caption">Terms & Conditions</StyledTypography>
 
-            <StyledTypography variant="caption">Become an Affliate</StyledTypography>
+            <StyledTypography sx={{position:"relative", left:-62}} variant="caption">Become an Affliate</StyledTypography>
 
-            <StyledTypography variant="caption">Billing Policy</StyledTypography>
+            <StyledTypography sx={{position:"relative", left:-12}}  variant="caption">Billing Policy</StyledTypography>
           </div>
          
-          <div style={{display:"flex",justifyContent:"space-around", }}>
+          <div style={{display:"grid",gap:"20px", marginLeft:"10%", gridTemplateColumns:"auto auto auto auto"}}>
          
             <StyledTypography variant="caption">JOIN US ON SOCIAL</StyledTypography>
+            <br></br>
 
 
          
+          </div>
+          <div  style={{display:"flex",marginLeft:"11%" }}>
+          <StyledTypography sx={{position:"relative", left:-12}}  variant="caption">Billing Policy</StyledTypography>
+          <StyledTypography sx={{position:"relative", left:-12}}  variant="caption">Billing Policy</StyledTypography>
+          <StyledTypography sx={{position:"relative", left:-12}}  variant="caption">Billing Policy</StyledTypography>
+
           </div>
          
         </Paper>
